36b9bd4b47aa1b66353845fb14e25d2b1b751be9
Mr Christian <feyemlionel@gmail.com>
2025-09-11 16:42:15 +0200

    adding qrcode verification

---
+++ src/environment/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://ton-backend.onrender.com',
+    payUnit: {
+        x_api_key: 'ta_clef_prod',
+        authorization: 'Bearer ton_token_prod',
+        baseUrl: 'https://api.payunit.net/api', // mode production
+    }
+};
---
+++ src/environment/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: false,
+    apiUrl: 'http://localhost:5000',
+    payUnit: {
+        x_api_key: 'ta_clef_test',
+        authorization: 'Bearer ton_token_test',
+        baseUrl: 'https://sandbox.payunit.net/api', // mode test
+    }
+};
